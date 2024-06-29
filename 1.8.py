f190c3087ac1604936ec073ff48a76c836023622
manhLV <manh132864@gmail.com>
2024-06-29 11:08:59 +0700

    commit

---
+++ 1.8.py
@@ -0,0 +1,9 @@
+import random
+
+#Số nguyên ngẫu nhiên từ 0 đến 9
+num1 = random.randint(0,9)
+print("Số nguyên ngẫu nhiên từ 0 đến 9:", num1)
+
+#Số nguyên ngẫu nhiên từ 10 đến 100
+num2 = random.randint(10,100)
+print("Số nguyên ngẫu nhiên từ 10 đến 100", num2)
